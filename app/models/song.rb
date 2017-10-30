709ae7782341ad724c5d18a5f67dc28e20aca768
aisayo <aisayo@icloud.com>
2017-10-30 23:03:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,8 +4,10 @@ class Song < ActiveRecord::Base
   def artist_name
     if self.artist
       self.artist.name
+    end
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by({:name => name})
   end
 end
